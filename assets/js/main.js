1d13d0d1aeb685562f68be07413167f97269e8bb
thanghn14 <thanghongocql@gmail.com>
2022-12-29 14:30:20 +0700

    Changes to be committed: 	modified:   assets/js/main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Dec 30, 2022 11:00:00").getTime();
+var countDownDate = new Date("Dec 29, 2022 14:40:00").getTime();
 
 var x = setInterval(function () {
   var now = new Date().getTime();
