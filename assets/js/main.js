4eac9ee8301cde09f572885199bd0770ea7f6d51
thanghn14 <thanghongocql@gmail.com>
2022-12-29 13:47:33 +0700

    Changes to be committed: 	modified:   assets/css/main.css 	new file:   assets/img/logo.jpg 	new file:   assets/img/logo.png 	modified:   assets/js/main.js 	modified:   index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
-var countDownDate = new Date("Dec 29, 2022 13:00:00").getTime();
+var countDownDate = new Date("Dec 29, 2022 14:50:00").getTime();
 
 var x = setInterval(function () {
   var now = new Date().getTime();
   var distance = countDownDate - now;
-  var weeks = Math.floor(distance / (1000 * 60 * 60 * 24 * 7));
+  var weeks = 4;
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  if (seconds <= 0) {
+  if (distance <= 0) {
     clearInterval(x);
     document.getElementById("week").innerHTML = 0;
     document.getElementById("days").innerHTML = 0;
